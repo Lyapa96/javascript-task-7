0179e62ed5a5608a4928b5c8ee33f20142bfaa12
lms <lms@skbkontur.ru>
2017-11-30 00:23:44 +0500

    make task

--- async.js
+++ async.js
@@ -1,13 +1,59 @@
 'use strict';
 
-exports.isStar = true;
+exports.isStar = false;
 exports.runParallel = runParallel;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
+ * @returns {Promise}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+
+    return new Promise((resolve) => {
+        if (jobs.length === 0) {
+            resolve([]);
+        }
+
+        let jobsResults = [];
+        let currentJobIndex = -1;
+
+        for (var i = 0; i < parallelNum; i++) {
+            runNextJob();
+        }
+
+        function runNextJob() {
+            currentJobIndex++;
+            if (currentJobIndex >= jobs.length) {
+                if (jobsResults.length === jobs.length) {
+                    resolve(jobsResults);
+
+                    return;
+                }
+
+                return;
+            }
+
+            var job = jobs[currentJobIndex];
+            runJob(job, currentJobIndex);
+        }
+
+        function runJob(currentJob, jobIndex) {
+            new Promise((runJobResolve, runJobReject) => {
+                currentJob().then(runJobResolve, runJobReject);
+                setTimeout(runJobReject, timeout, new Error('Promise timeout'));
+            })
+                .then(result => save(result, jobIndex))
+                .catch(result => save(result, jobIndex));
+        }
+
+        function save(result, jobIndex) {
+            jobsResults[jobIndex] = result;
+
+            runNextJob();
+        }
+    });
 }
+
+
